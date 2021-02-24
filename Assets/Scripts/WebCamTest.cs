4bd52988cfc6d9697db11b667e6c657eeb146974
Jakob Anderson <jakob.anderson@uoit.net>
2021-02-24 14:35:58 -0500

    Implemented Vuforia, set up a basic target recognition in the sample scene

---
+++ Assets/Scripts/WebCamTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class WebCamTest : MonoBehaviour
+{
+    private WebCamTexture wct;
+    private Material myMaterial;
+
+    private void Awake()
+    {
+        Debug.Log(Screen.height + ", " + Screen.width);
+
+        myMaterial = GetComponent<Renderer>().material;
+        wct = new WebCamTexture();
+
+        myMaterial.mainTexture = wct;
+
+        Vector3 localScale = transform.localScale;
+        localScale.z = (float)Screen.height / (float)Screen.width;
+        transform.localScale = localScale;
+    }
+
+    private void OnEnable()
+    {
+        wct.Play();
+    }
+
+    private void OnDisable()
+    {
+        wct.Stop();
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(myMaterial);
+        Destroy(wct);
+    }
+}
