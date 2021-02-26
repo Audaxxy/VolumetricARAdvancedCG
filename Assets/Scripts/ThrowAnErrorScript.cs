2a886f3d4822e9215b15bc4cfae25360d77cb69d
Jakob Anderson <jakob.anderson@uoit.net>
2021-02-26 01:00:17 -0500

    Debugging scripts added

---
+++ Assets/Scripts/ThrowAnErrorScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowAnErrorScript : MonoBehaviour
+{
+    public void ThrowDebugError(string errorDetails)
+    {
+        Debug.LogError(errorDetails);
+    }
+}
