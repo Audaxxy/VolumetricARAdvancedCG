d777013d7e7458745d325a5483f66df3c29fed99
Unknown <robert.andersen@uoit.net>
2021-03-22 15:47:45 -0400

    Made new project and added steam VR plugin. Created Buttons.
    Buttons able to be pressed. Yellow takes player to new scene, red is the quit button.
    
    Player can teleport in VR space and is able to move their hands and head etc.

---
+++ Volumetric VR/Assets/_Scripts/MenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public List<string> buttonNames;
+    public string sceneName;
+
+    //When Button Top hits TriggerPlate do something based on Button name.
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == buttonNames[0])
+        {
+            QuitButton();
+        }
+        else if(col.gameObject.name == buttonNames[1])
+        {
+            ButtonOne();
+        }
+    }
+    void ButtonOne()
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    void ButtonTwo()
+    {
+        
+    }
+
+    //etc
+
+    void QuitButton(){
+        Application.Quit();
+    }
+}
