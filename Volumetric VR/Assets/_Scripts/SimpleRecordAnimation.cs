1613357ff2befeefbef10ad96b083df83a2bf4c0
Jakob Anderson <jakob.anderson@uoit.net>
2021-03-29 16:37:14 -0400

    Upgraded project and made a simple animation recorder in play mode

---
+++ Volumetric VR/Assets/_Scripts/SimpleRecordAnimation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class SimpleRecordAnimation : MonoBehaviour
+{
+    [SerializeField]
+    public AnimationClip clipToWriteTo;
+
+    private GameObjectRecorder gor;
+
+    private void Start()
+    {
+        clipToWriteTo.ClearCurves();
+
+        gor = new GameObjectRecorder(gameObject);
+        gor.BindComponent(transform);
+    }
+
+    private void LateUpdate()
+    {
+        if (clipToWriteTo == null)
+        {
+            Debug.LogError("ERROR! NO CLIP!");
+            return;
+        }
+
+        gor.TakeSnapshot(Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        if (gor.isRecording)
+        {
+            gor.SaveToClip(clipToWriteTo);
+        }
+
+        Destroy(gor);
+    }
+}
